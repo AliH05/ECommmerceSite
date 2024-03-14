3cc611b681193ee13c2698dccd7480be7fc1e9f9
louy755 <lumberjack1695@yahoo.com>
2024-03-14 17:04:08 -0500

    Created Product model and added functionality

---
+++ ECommmerceSite/Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using ECommmerceSite.Models;
+using ECommmerceSite.Models.Identity;
+using ECommmerceSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommmerceSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _productService;
+        private readonly UserService _userService;
+        private readonly AppUser _currentUser;
+        public ProductController(ProductService productService, UserService userService)
+        {
+            _productService = productService;
+            _userService = userService;
+            _currentUser = userService.GetCurrentUser();
+        }
+
+        public IActionResult Index()
+        {
+            var Products = _productService.GetAllProducts();
+            return View(Products);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            product.UserID = _currentUser.Id;
+            product.CompanyID = _currentUser.CompanyId;
+            var createdProduct = _productService.Create(product);
+            if (createdProduct)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Update(int productId)
+        {
+            var product = _productService.GetProductByID(productId);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            var createdProduct = _productService.Update(product);
+            if (createdProduct)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int productId)
+        {
+            var deletedProduct = _productService.Delete(productId);
+            return RedirectToAction("Index");
+        }
+    }
+}
